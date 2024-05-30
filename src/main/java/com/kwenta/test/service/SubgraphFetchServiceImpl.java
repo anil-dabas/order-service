63b3045479cdff4adc024f95e704a7a091a800be
Anil dabas <anildabas0219@gmail.com>
2024-05-31 00:25:03 +0800

    Adding changes to read orders from chain

--- src/main/java/com/kwenta/test/service/SubgraphFetchServiceImpl.java
+++ src/main/java/com/kwenta/test/service/SubgraphFetchServiceImpl.java
@@ -8,19 +8,9 @@ import com.kwenta.test.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.web3j.abi.TypeReference;
-import org.web3j.abi.datatypes.Address;
-import org.web3j.abi.datatypes.Bool;
-import org.web3j.abi.datatypes.Event;
-import org.web3j.abi.datatypes.generated.Bytes32;
-import org.web3j.abi.datatypes.generated.Int256;
-import org.web3j.abi.datatypes.generated.Uint256;
-import org.web3j.abi.datatypes.generated.Uint8;
-import org.web3j.protocol.core.methods.response.Log;
-import org.web3j.tx.Contract;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.List;
 
 import static com.kwenta.test.constant.Constants.QUERY;
 import static com.kwenta.test.constant.Constants.SUBGRAPH_URL;
@@ -43,17 +33,8 @@ public class SubgraphFetchServiceImpl {
         String response = restTemplate.postForObject(SUBGRAPH_URL, QUERY, String.class);
 
         JsonNode root = objectMapper.readTree(response);
-        JsonNode orders = root.path("data").path("smartMarginOrders");
-        // TODO need to convert the values to Proper string and integer values instead of storing Hexadecimals in database
-        for (JsonNode orderNode : orders) {
-            Order order = new Order();
-            order.setId(orderNode.path("id").asText());
-            order.setAccount(orderNode.path("account").asText());
-            order.setOrderType(orderNode.path("orderType").asText());
-            order.setMarketKey(orderNode.path("marketKey").asText());
-            order.setRecordTrade(orderNode.path("recordTrade").asText());
-            order.setFeesPaid(new BigDecimal(orderNode.path("feesPaid").asText()));
-            orderRepository.save(order);
-        }
+        JsonNode ordersNode = root.path("data").path("smartMarginOrders");
+        List<Order> orders = Arrays.asList(objectMapper.treeToValue(ordersNode, Order[].class));
+        orderRepository.saveAll(orders);
     }
 }
